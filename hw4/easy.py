dffd590e83b33aa782275cad7e9a8dac175d2a06
YUMVOLKOVA <60215607+YUMVOLKOVA@users.noreply.github.com>
2022-03-09 22:01:16 +0300

    init easy

--- hw4/easy.py
+++ hw4/easy.py
@@ -29,35 +29,35 @@ params = {'n': 10,
 if __name__ == '__main__':
     result_dict = {}
 
-    start_1 = time.time()
+    start_1 = time.perf_counter()
     for i in range(params['n_flows']):
         fibonacci(params['n'])
-    end_1 = time.time()
+    end_1 = time.perf_counter()
     total_time_1 = end_1 - start_1
     result_dict['Sequential'] = str(total_time_1)
     print(f'done with Sequential, result: {total_time_1}')
 
-    start_2 = time.time()
+    start_2 = time.perf_counter()
     list_threads = [Thread(target=fibonacci, args=(params['n'],)) for i in range(params['n_flows'])]
     for i in list_threads:
         i.start()
     for i in list_threads:
         i.join()
-    end_2 = time.time()
+    end_2 = time.perf_counter()
     total_time_2 = end_2 - start_2
     result_dict['Thread'] = str(total_time_2)
     print(f'done with Thread, result: {total_time_2}')
 
-    start_3 = time.time()
+    start_3 = time.perf_counter()
     list_process = [Process(target=fibonacci, args=(params['n'],)) for i in range(params['n_flows'])]
     for i in list_process:
         i.start()
     for i in list_process:
         i.join()
-    end_3 = time.time()
+    end_3 = time.perf_counter()
     total_time_3 = end_3 - start_3
     result_dict['Process'] = str(total_time_3)
     print(f'done with Process, result: {total_time_3}')
 
     path_ = 'artifacts/easy.txt'
-    save_results(path_, result_dict)
+    save_results(path_, **result_dict)
