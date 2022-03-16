44b75b4bbf4387635fc58c15394ce5e0820d7d96
YUMVOLKOVA <60215607+YUMVOLKOVA@users.noreply.github.com>
2022-03-16 22:09:41 +0300

    init easy

--- hw5/easy.py
+++ hw5/easy.py
@@ -1,6 +1,13 @@
-import aiohttp
+# import aiohttp
 import argparse
 import asyncio
+import pip
+
+try:
+    import aiohttp
+except:
+    !pip install aiohttp
+    import aiohttp
 
 #####
 # из лекции
@@ -32,7 +39,7 @@ async def download_site(url, session, path):
 async def download_all_sites(n, path):
     async with aiohttp.ClientSession() as session:
         tasks = []
-        for url in range(n):
+        for url in range(int(n)):
             task = asyncio.create_task(download_site(str(params['url']) + str(url) + '/200', session, path))
             tasks.append(task)
     return await asyncio.gather(*tasks)
@@ -40,8 +47,8 @@ async def download_all_sites(n, path):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-n', default=1, help="number of files")
-    parser.add_argument('-path', default="artifacts/easy", help="saving path")
+    parser.add_argument('-n', default=1, help='number of files')
+    parser.add_argument('-path', default='artifacts/easy', help='saving path')
     args = parser.parse_args()
 
     asyncio.run(download_all_sites(args.n, args.path))
