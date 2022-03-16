270494db97525ebabd47f7a4ee8523e79d848a7f
YUMVOLKOVA <60215607+YUMVOLKOVA@users.noreply.github.com>
2022-03-16 21:52:26 +0300

    init easy

--- hw5/easy.py
+++ hw5/easy.py
@@ -1,22 +1,47 @@
-import asyncio
 import aiohttp
+import argparse
+import asyncio
 
-# из лекции
 #####
-async def download_site(url, seccion):
-    async with session.get(url) as response:
-        print(f"Read {response.content.total_bytes} from {url}")
-        return response.content.total_bytes
+# из лекции
+# async def download_site(url, session):
+#     async with session.get(url) as response:
+#         print(f"Read {response.content.total_bytes} from {url}")
+#         return response.content.total_bytes
+#
+# async def download_all_sites(sites):
+#     async with aiohttp.ClientSession() as session:
+#         tasks = []
+#         for url in sites:
+#         task = asyncio.create_task(download_site(url, session))
+#         tasks.append(task)
+#     return await asyncio.gather(*tasks)
+#####
 
-asunc def download_all_sites(sites):
+params = {'url': 'https://picsum.photos/id/',
+          'dream': 1}
+
+
+async def download_site(url, session, path):
+    async with session.get(url) as response:
+        with open(f'{path}/{url}.png', "bw") as f:
+            f.write((await response.content.read()))
+        time.sleep(params['dream'])
+
+
+async def download_all_sites(n, path):
     async with aiohttp.ClientSession() as session:
         tasks = []
-        for url in sites:
-        task = asyncio.create_task(download_site(url, session))
-        tasks.append(task)
+        for url in range(n):
+            task = asyncio.create_task(download_site(str(params['url']) + str(url) + '/200', session, path))
+            tasks.append(task)
     return await asyncio.gather(*tasks)
-#####
 
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop)_
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-n', default=1, help="number of files")
+    parser.add_argument('-path', default="artifacts/easy", help="saving path")
+    args = parser.parse_args()
+
+    asyncio.run(download_all_sites(args.n, args.path))
